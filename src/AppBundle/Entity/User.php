1dad4083a35b8d2addcc4d335728bdb3899ad170
mac <bsuir.dmitriy.dronov@gmail.com>
2017-09-15 22:30:25 +0300

    Deleted space string in User.php

--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -8,7 +8,6 @@
 
 namespace AppBundle\Entity;
 
-
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\AdvancedUserInterface;
 use Symfony\Component\Validator\Constraints as Assert;

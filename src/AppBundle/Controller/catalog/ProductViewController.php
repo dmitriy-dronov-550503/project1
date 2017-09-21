b44ebe3dcea5056d6567cdc5a646c3001d59f88e
mac <bsuir.dmitriy.dronov@gmail.com>
2017-09-21 23:51:04 +0300

    Added small ajax example.

--- src/AppBundle/Controller/catalog/ProductViewController.php
+++ src/AppBundle/Controller/catalog/ProductViewController.php
@@ -3,23 +3,37 @@
 namespace AppBundle\Controller\catalog;
 
 use AppBundle\Entity\Category;
+use AppBundle\Entity\Product;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ProductViewController extends Controller
 {
-    // there should be uniq URLs for each product in Rout
+
     /**
      * @Route("/product_view", name="product_view")
      */
-    public function indexAction(Request $request)
+    public function productViewAction(Request $request)
     {
-        $repository = $this->getDoctrine()->getRepository(Category::class);
-
+        //$repository = $this->getDoctrine()->getRepository(Product::class);
         // replace this example code with whatever you need
-        return $this->render(
-            'catalog/productView.html.twig'
-            );
+        return $this->render('catalog/productView.html.twig'
+        );
     }
+
+    /**
+     * @Route("product_view/action", name="product_view_action")
+     */
+    public function dataActrion(Request $request) {
+        return new Response(
+            'Значение переменной 1:<br/>
+            <strong>'.$request->request->get('data_1').'</strong>
+            <hr/>
+            Значение переменной 2:<br/>
+            <strong>'.$request->request->get('data_2').'</strong>'
+        );
+    }
+
 }
